5186c68d904c172486f13f122adaa6704b2d4f7f
in-vidyabor <vidyabor@thoughtworks.com>
2018-07-17 11:15:09 +0530

    Adding functional handler

---
+++ src/main/java/com/tw/springbootwebfluxvsspringmvc/reactive/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.tw.springbootwebfluxvsspringmvc.reactive.config;
+
+import com.tw.springbootwebfluxvsspringmvc.reactive.handler.RateHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+    @Bean
+    public RouterFunction<ServerResponse> routeGetRates(RateHandler rateHandler) {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/reactive/rates"), rateHandler::getRates);
+
+    }
+}
+
